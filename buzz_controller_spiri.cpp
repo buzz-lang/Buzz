89142bebb21ce5866c902c1fdc7d5e9686c4a98d
Carlo Pinciroli <cpinciro@ulb.ac.be>
2015-06-22 18:48:55 -0400

    Fixed speed factor in Spiri controller.

--- buzz_controller_spiri.cpp
+++ buzz_controller_spiri.cpp
@@ -75,7 +75,7 @@ void CBuzzControllerSpiri::SetDirection(const CVector3& c_heading) {
    CVector3 cDir = c_heading;
    if(cDir.SquareLength() > 0.01f) {
       cDir.Normalize();
-      cDir *= 0.1;
+      cDir *= 0.01;
    }
    m_pcPropellers->SetRelativePosition(cDir);
 }
