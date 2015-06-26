b0a766cf3217a6c482f2e4baecaf4af01fe9858e
Carlo Pinciroli <cpinciro@ulb.ac.be>
2015-06-26 17:53:49 -0400

    Added CMakeFile structure, with build checks and packaging information.

--- src/cmake/BuzzBuildFlags.cmake
+++ src/cmake/BuzzBuildFlags.cmake
@@ -1,16 +1,28 @@
-# Debug flags
-set(CMAKE_C_FLAGS "-std=c99")
+#
+# Get information about the current processor
+#
+execute_process(
+  COMMAND uname -m
+  COMMAND tr -d '\n'
+  OUTPUT_VARIABLE BUZZ_PROCESSOR_ARCH)
+
+#
+# General compilation flags
+#
+set(CMAKE_C_FLAGS   "-Wall -std=c99")
+set(CMAKE_CXX_FLAGS "-Wall")
 if(NOT APPLE)
-  set(CMAKE_C_FLAGS_DEBUG             "-g -ggdb3")
-  set(CMAKE_CXX_FLAGS_DEBUG           "-g -ggdb3")
-  set(CMAKE_EXE_LINKER_FLAGS_DEBUG    "-ggdb3")
-  set(CMAKE_MODULE_LINKER_FLAGS_DEBUG "-ggdb3")
-  set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "-ggdb3")
+  set(BUZZ_FLAGS_DEBUG "-ggdb3")
   add_definitions(-D_GNU_SOURCE)
 else(NOT APPLE)
-  set(CMAKE_C_FLAGS_DEBUG             "-g -gdwarf-4")
-  set(CMAKE_CXX_FLAGS_DEBUG           "-g -gdwarf-4")
-  set(CMAKE_EXE_LINKER_FLAGS_DEBUG    "-gdwarf-4")
-  set(CMAKE_MODULE_LINKER_FLAGS_DEBUG "-gdwarf-4")
-  set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "-gdwarf-4")  
+  set(BUZZ_FLAGS_DEBUG "-gdwarf-4")
 endif(NOT APPLE)
+set(CMAKE_C_FLAGS_DEBUG             ${BUZZ_FLAGS_DEBUG})
+set(CMAKE_C_FLAGS_RELEASE           "-Os -DNDEBUG")
+set(CMAKE_C_FLAGS_RELWITHDEBINFO    "${BUZZ_FLAGS_DEBUG} -Os -DNDEBUG")
+set(CMAKE_CXX_FLAGS_DEBUG           ${CMAKE_C_FLAGS_DEBUG})
+set(CMAKE_CXX_FLAGS_RELEASE         ${CMAKE_C_FLAGS_RELEASE})
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  ${CMAKE_C_FLAGS_RELWITHDEBINFO})
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG    "-ggdb3")
+set(CMAKE_MODULE_LINKER_FLAGS_DEBUG "-ggdb3")
+set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "-ggdb3")
