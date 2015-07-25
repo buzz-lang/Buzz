b7b34182285f0e5387afebdc6fb2973b11407b04
Carlo Pinciroli <cpinciro@ulb.ac.be>
2015-07-25 19:08:51 -0400

    Improved testbuzz utility.
    - it accepts --trace to show the stack trace during the execution
    - implements the function 'print' to make testing/debugging easier

--- src/testing/testbuzz.c
+++ src/testing/testbuzz.c
@@ -5,6 +5,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void usage(const char* path, int status) {
+   fprintf(stderr, "Usage:\n\t%s [--trace] <file.bo>\n\n", path);
+   exit(status);
+}
+
 void dump(buzzvm_t vm) {
    int64_t i;
    fprintf(stdout, "============================================================\n");
@@ -43,21 +48,61 @@ void dump(buzzvm_t vm) {
    fprintf(stdout, "============================================================\n\n");
 }
 
-int hook(buzzvm_t vm) {
-   fprintf(stdout, "Hook called!\n\n");
-   buzzvm_pushf(vm, 17.0);
-   return buzzvm_ret1(vm);
+int print(buzzvm_t vm) {
+   for(int i = 1; i < buzzdarray_size(vm->lsyms->syms); ++i) {
+      buzzvm_lload(vm, i);
+      buzzobj_t o = buzzvm_stack_at(vm, 1);
+      buzzvm_pop(vm);
+      switch(o->o.type) {
+         case BUZZTYPE_NIL:
+            fprintf(stdout, "[nil]");
+            break;
+         case BUZZTYPE_INT:
+            fprintf(stdout, "%d", o->i.value);
+            break;
+         case BUZZTYPE_FLOAT:
+            fprintf(stdout, "%f", o->f.value);
+            break;
+         case BUZZTYPE_TABLE:
+            fprintf(stdout, "[table with %d elems]", (buzzdict_size(o->t.value)));
+            break;
+         case BUZZTYPE_CLOSURE:
+            if(o->c.value.isnative)
+               fprintf(stdout, "[n-closure @%d]", o->c.value.ref);
+            else
+               fprintf(stdout, "[c-closure @%d]", o->c.value.ref);
+            break;
+         case BUZZTYPE_STRING:
+            fprintf(stdout, "%s", o->s.value.str);
+            break;
+         case BUZZTYPE_USERDATA:
+            fprintf(stdout, "[userdata @%p]", o->u.value);
+            break;
+         default:
+            break;
+      }
+   }
+   fprintf(stdout, "\n");
+   return buzzvm_ret0(vm);
 }
 
 int main(int argc, char** argv) {
+   char* fname;
+   int trace = 0;
    /* Parse command line */
-   if(argc != 2) {
-      fprintf(stderr, "Usage:\n\t%s <file.bo>\n\n", argv[0]);
-      return 0;
+   if(argc < 2 || argc > 3) usage(argv[0], 0);
+   if(argc == 2) fname = argv[1];
+   else {
+      fname = argv[2];
+      if(strcmp(argv[1], "--trace") != 0) {
+         fprintf(stderr, "error: %s: unrecognized option '%s'\n", argv[0], argv[1]);
+         usage(argv[0], 1);
+      }
+      trace = 1;
    }
    /* Open file */
-   int fd = open(argv[1], O_RDONLY);
-   if(fd < 0) perror(argv[1]);
+   int fd = open(fname, O_RDONLY);
+   if(fd < 0) perror(fname);
    /* Read data */
    size_t bcode_size = lseek(fd, 0, SEEK_END);
    lseek(fd, 0, SEEK_SET);
@@ -66,7 +111,7 @@ int main(int argc, char** argv) {
    size_t tot = 0;
    while(tot < bcode_size) {
       rd = read(fd, bcode_buf + tot, bcode_size - tot);
-      if(rd < 0) perror(argv[1]);
+      if(rd < 0) perror(fname);
       tot += rd;
    }
    close(fd);
@@ -74,21 +119,22 @@ int main(int argc, char** argv) {
    buzzvm_t vm = buzzvm_new(1);
    /* Set byte code */
    buzzvm_set_bcode(vm, bcode_buf, bcode_size);
-   /* Register hook function */
-   buzzvm_pushs(vm, buzzvm_string_register(vm, "hook"));
-   buzzvm_pushcc(vm, buzzvm_function_register(vm, hook));
+   /* Register hook functions */
+   buzzvm_pushs(vm, buzzvm_string_register(vm, "print"));
+   buzzvm_pushcc(vm, buzzvm_function_register(vm, print));
    buzzvm_gstore(vm);
    /* Run byte code and dump state */
-   do dump(vm);
+   do if(trace) dump(vm);
    while(buzzvm_step(vm) == BUZZVM_STATE_READY);
    if(vm->state == BUZZVM_STATE_DONE) {
-      dump(vm);
+      if(trace) dump(vm);
       fprintf(stdout, "%s: execution terminated correctly\n\n",
-              argv[1]);
+              fname);
    }
    else {
+      if(trace) dump(vm);
       fprintf(stderr, "%s: execution terminated abnormally: %s\n\n",
-              argv[1],
+              fname,
               buzzvm_error_desc[vm->error]);
    }
    /* Destroy VM */
