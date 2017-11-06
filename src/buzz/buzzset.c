83f8dc7bdc7c6f87dbe36381d248d30344a6db4a
Carlo Pinciroli <cpinciro@ulb.ac.be>
2017-11-06 18:13:28 -0500

    Added Buzz set data structure.

---
+++ src/buzz/buzzset.c
@@ -0,0 +1,234 @@
+#include "buzzset.h"
+#include <stdlib.h>
+#include <string.h>
+
+/****************************************/
+/****************************************/
+
+struct buzzset_tree_s {
+   void* data;
+   struct buzzset_tree_s* left;
+   struct buzzset_tree_s* right;
+};
+typedef struct buzzset_tree_s* buzzset_tree_t;
+
+/****************************************/
+/****************************************/
+
+/* Creates a new tree node */
+static buzzset_tree_t buzzset_tree_new(buzzset_t s,
+                                       const void* data) {
+   buzzset_tree_t x = (buzzset_tree_t)malloc(sizeof(struct buzzset_tree_s));
+   x->data = malloc(s->data_size);
+   memcpy(x->data, data, s->data_size);
+   x->left = NULL;
+   x->right = NULL;
+   return x;
+}
+
+/* Destroys a node and its children */
+static void buzzset_tree_destroy(buzzset_t s,
+                                 buzzset_tree_t n) {
+   if(!n) return;
+   if(n->left) buzzset_tree_destroy(s, n->left);
+   if(n->right) buzzset_tree_destroy(s, n->right);
+   if(s->dstryf) s->dstryf(n->data, NULL);
+   free(n);
+}
+
+/* Inserts a new node in the tree */
+static buzzset_tree_t buzzset_tree_insert(buzzset_t s,
+                                          buzzset_tree_t n,
+                                          const void* data,
+                                          int* added) {
+   /* Insert element here */
+   if(!n) {
+      *added = 1;
+      return buzzset_tree_new(s, data);
+   }
+   else {
+      int res = s->cmpf(data, n->data);
+      /* Insert element on the left */
+      if(res < 0)
+         n->left  = buzzset_tree_insert(s, n->left, data, added);
+      /* Insert element on the right */
+      else if(res > 0)
+         n->right = buzzset_tree_insert(s, n->right, data, added);
+      else
+         /* If comparison is 0, the element has already been inserted,
+          * nothing to do */
+         *added = 0;
+      /* Return current tree */
+      return n;
+   }
+}
+
+/****************************************/
+/****************************************/
+
+/* Visits every node in the tree and executes the given function */
+static void buzzset_tree_foreach(buzzset_tree_t n,
+                                 buzzset_elem_funp fun,
+                                 void* params) {
+   if(!n) return;
+   fun(n->data, params);
+   if(n->left)  buzzset_tree_foreach(n->left, fun, params);
+   if(n->right) buzzset_tree_foreach(n->right, fun, params);
+}
+
+/****************************************/
+/****************************************/
+
+/* Finds the smallest node in a tree */
+static buzzset_tree_t buzzset_tree_find_min(buzzset_tree_t n) {
+   /* This node has no smaller child */
+   if(n->left == NULL) return n;
+   /* Go to the smaller child */
+   return buzzset_tree_find_min(n->left);
+}
+
+/****************************************/
+/****************************************/
+
+/* Removes a node with the given data from the tree */
+static buzzset_tree_t buzzset_tree_remove(buzzset_t s,
+                                          buzzset_tree_t n,
+                                          const void* data,
+                                          int* removed) {
+   /* If the tree is empty, nothing to do */
+   if(!n) {
+      *removed = 0;
+      return NULL;
+   }
+   /* Call comparison function */
+   int res = s->cmpf(data, n->data);
+   /* If the id is smaller than the current id, remove on the left */
+   if(res < 0)
+      n->left = buzzset_tree_remove(s, n->left, data, removed);
+   /* If the id is greater than the current id, remove on the right */
+   else if(res > 0)
+      n->right = buzzset_tree_remove(s, n->right, data, removed);
+   else {
+      /* We found the node to remove */
+      *removed = 1;
+      /* Get rid of the data */
+      if(s->dstryf) s->dstryf(n->data, NULL);
+      /* If the node has no children, simply remove it */
+      if(!n->left && !n->right) {
+         free(n);
+         n = NULL;
+      }
+      /* If the element has one child, put the child in place of this
+       * tree, and remove the tree */
+      else if(!n->right) {
+         buzzset_tree_t x = n;
+         n = n->left;
+         free(x);
+      }
+      else if(!n->left) {
+         buzzset_tree_t x = n;
+         n = n->right;
+         free(x);
+      }
+      /* Both children exist for this node */
+      else {
+         /* Look for the minimum id on the right of the node to eliminate */
+         buzzset_tree_t m = buzzset_tree_find_min(n->right);
+         /* Copy its value over the current root */
+         n->data = m->data;
+         /* Delete the minimum node */
+         n->right = buzzset_tree_remove(s, n->right, m->data, removed);
+      }
+   }
+   return n;
+}
+
+/****************************************/
+/****************************************/
+
+/* Removes a node with the given data from the tree */
+static buzzset_tree_t buzzset_tree_find(buzzset_t s,
+                                        buzzset_tree_t n,
+                                        const void* data) {
+   /* If the tree is empty, nothing to do */
+   if(!n) return NULL;
+   /* Call comparison function */
+   int res = s->cmpf(data, n->data);
+   /* If the data is smaller than the current data, recur on the left */
+   if(res < 0)
+      return buzzset_tree_find(s, n->left, data);
+   /* If the data is greater than the current data, recur on the right */
+   else if(res > 0)
+      return buzzset_tree_find(s, n->right, data);
+   /* We found the node to remove */
+   return n;
+}
+
+/****************************************/
+/****************************************/
+
+buzzset_t buzzset_new(uint32_t elem_size,
+                      buzzset_elem_cmpp elem_cmp,
+                      buzzset_elem_funp elem_destroy) {
+   buzzset_t s = (buzzset_t)malloc(sizeof(struct buzzset_s));
+   s->data = NULL;
+   s->size = 0;
+   s->dstryf = elem_destroy;
+   s->cmpf = elem_cmp;
+   s->data_size = elem_size;
+   return s;
+}
+
+/****************************************/
+/****************************************/
+
+void buzzset_destroy(buzzset_t* s) {
+   /* Get rid of every data element */
+   buzzset_tree_destroy(*s, (*s)->data);
+   /* Get rid of the rest */
+   free(*s);
+   /* Set s to NULL */
+   *s = NULL;
+}
+
+/****************************************/
+/****************************************/
+
+void buzzset_insert(buzzset_t s,
+                    const void* data) {
+   int added;
+   s->data = buzzset_tree_insert(s, s->data, data, &added);
+   if(added) s->size++;
+}
+
+/****************************************/
+/****************************************/
+
+void buzzset_remove(buzzset_t s,
+                    const void* data) {
+   int removed;
+   s->data = buzzset_tree_remove(s, s->data, data, &removed);
+   if(removed) s->size--;
+}
+
+/****************************************/
+/****************************************/
+
+void* buzzset_get(buzzset_t s,
+                  const void* data) {
+   buzzset_tree_t res = buzzset_tree_find(s, s->data, data);
+   if(!res) return NULL;
+   return res->data;
+}
+
+/****************************************/
+/****************************************/
+
+void buzzset_foreach(buzzset_t s,
+                     buzzset_elem_funp fun,
+                     void* params) {
+   buzzset_tree_foreach(s->data, fun, params);
+}
+
+/****************************************/
+/****************************************/
---
+++ src/buzz/buzzset.h
@@ -0,0 +1,116 @@
+#ifndef BUZZSET_H
+#define BUZZSET_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+   /*
+    * Function pointer for an element-wise function:
+    *
+    * void f(void* data, void* params)
+    *
+    * This function pointer is used to destroy elements by
+    * buzzset_destroy() and in methods such as buzzset_foreach().
+    */
+   typedef void (*buzzset_elem_funp)(void* data, void* params);
+
+   /*
+    * Function pointer to compare set elements.
+    *
+    * int f(const void* a, const void* b)
+    *
+    * The function must return:
+    * -1 if *a < *b
+    * 0  if *a == *b
+    * 1  if *a > *b
+    */
+   typedef int (*buzzset_elem_cmpp)(const void* a, const void* b);
+
+   /*
+    * The Buzz set.
+    */
+   struct buzzset_s {
+      void* data;               // Actual data
+      uint32_t size;            // Number of inserted elements
+      buzzset_elem_funp dstryf; // Element destroy function
+      buzzset_elem_cmpp cmpf;   // Element comparison function
+      uint32_t data_size;       // Size of data element.
+   };
+   typedef struct buzzset_s* buzzset_t;
+
+   /*
+    * Creates a new Buzz set.
+    * @param elem_cmp     The element comparison function.
+    * @param elem_destroy The function to destroy an element. Can be NULL.
+    * @return A new set.
+    */
+   extern buzzset_t buzzset_new(uint32_t elem_size,
+                                buzzset_elem_cmpp elem_cmp,
+                                buzzset_elem_funp elem_destroy);
+   
+   /*
+    * Destroys a set.
+    * Internally calls s.elem_destroy(), if not NULL.
+    * @param s The set.
+    */
+   extern void buzzset_destroy(buzzset_t* s);
+
+   /*
+    * Inserts an element.
+    * The element must be passed as a pointer. The pointed data is copied
+    * into the data structure.
+    * @param s The set.
+    * @param data A pointer to the element to add.
+    */
+   extern void buzzset_insert(buzzset_t s,
+                              const void* data);
+
+   /*
+    * Removes the passed element.
+    * @param s The set.
+    * @param data A pointer to the element to remove.
+    */
+   extern void buzzset_remove(buzzset_t s,
+                              const void* data);
+
+   /*
+    * Gets an element.
+    * @param s The set.
+    * @param data The element to find.
+    * @return The position of the found element, or NULL.
+    */
+   extern void* buzzset_get(buzzset_t s,
+                            const void* data);
+
+   /*
+    * Applies a function to each element of the set.
+    * @param s The set.
+    * @param fun The function to apply to each element.
+    * @param params A data structure to pass along.
+    */
+   extern void buzzset_foreach(buzzset_t s,
+                               buzzset_elem_funp fun,
+                               void* params);
+   
+#ifdef __cplusplus
+}
+#endif
+
+/*
+ * Returns the size of the set.
+ * @param s The set.
+ * @return The size of the set.
+ */
+#define buzzset_size(s) (s)->size
+
+/*
+ * Returns <tt>true</tt> if the set is empty.
+ * @param s The set.
+ * @return <tt>true</tt> if the set is empty.
+ */
+#define buzzset_isempty(s) (buzzset_size(s) == 0)
+
+#endif
