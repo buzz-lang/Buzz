b92b8d9017a94942cc8bd179559a8607156d1e7b
Carlo Pinciroli <carlo@pinciroli.net>
2024-11-08 16:04:35 -0500

    Fixed bug in buzzvm_function_call().
    The bug was subtle. The function expects the stack to have the arguments pushed
    in order. Internally, it looks for the closure corresponding to the passed
    string, which leaves the closure on top of the stack. However, a call to
    buzzvm_closure_call is done that requires the closure to be after the arguments,
    not on top of them, in the stack. The happened when the closure was inserted
    before the arguments. Rather than passing a pointer to pointer to buzzobj_t, the
    function passed a straight pointer to buzzobj_t. This made it so memcpy would
    copy the wrong data into the array slot - rather than the pointer to buzzobj_t
    that stores the closure, it was copying the first 8 bytes of the closure data itself.

--- src/buzz/buzzvm.c
+++ src/buzz/buzzvm.c
@@ -1039,27 +1039,27 @@ buzzvm_state buzzvm_function_call(buzzvm_t vm,
    /* Get associated symbol */
    buzzvm_gload(vm);
    /* Make sure it's a closure */
-   if(buzzvm_stack_at(vm, 1)->o.type == BUZZTYPE_NIL) {
+   buzzobj_t closure = buzzvm_stack_at(vm, 1);
+   if(closure->o.type == BUZZTYPE_NIL) {
       buzzvm_seterror(vm,
                       BUZZVM_ERROR_TYPE,
                       "cannot find function '%s()'",
                       fname);
       return vm->state;
    }
-   if(buzzvm_stack_at(vm, 1)->o.type != BUZZTYPE_CLOSURE) {
+   if(closure->o.type != BUZZTYPE_CLOSURE) {
       buzzvm_seterror(vm,
                       BUZZVM_ERROR_TYPE,
                       "function '%s()': expected closure, got %s",
                       fname,
-                      buzztype_desc[buzzvm_stack_at(vm, 1)->o.type]
-         );
+                      buzztype_desc[closure->o.type]);
       return vm->state;
    }
    /* Move closure before arguments */
    if(argc > 0) {
       buzzdarray_insert(vm->stack,
                         buzzdarray_size(vm->stack) - argc - 1,
-                        buzzvm_stack_at(vm, 1));
+                        &closure);
       buzzvm_pop(vm);
    }
    /* Call the closure */
